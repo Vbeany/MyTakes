586002481adf5aaad26f3cabaffc67ce80ec9774
Venus Nguyen <89676581+Vbeany@users.noreply.github.com>
2024-07-26 21:06:01 -0400

    Assets pushed

--- MyTakes/app/index.tsx
+++ MyTakes/app/index.tsx
@@ -5,7 +5,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">MyTakes</Text>
+      <Text className="text-3xl font-pblack">MyTakes</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue'}}> Go to Profile</Link>
     </View>
